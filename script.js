319c5fa7beccf051e0c4e02711d847e8b7d8eaa3
Luciano Candotti <candotti@Mac-de-Luciano.local>
2025-01-17 16:50:12 -0300

    solucionado tema dimensión de divs

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ en la que su cantidad se ajusta a la ventana del ordenador */
 
 
 //tomamos las dimensiones de la ventana del usuario
-const ancho = window.innerWidth / 100;
-const alto = window.innerHeight / 100;
-const resolucion = alto * ancho;
-console.log(alto);
+const divwidth = Math.round(window.innerWidth / 10);
+const filas = window.innerHeight / divwidth;
+// const ancho = window.innerWidth / 100;
+// const alto = window.innerHeight / 100;
+const resolucion = 10 * filas;
+console.log(divwidth,resolucion);
+
 
 
 
@@ -23,5 +26,16 @@ for (let step = 0; step < resolucion; step++) {
     
     container.appendChild(div); /* ponemos a div como hijo de container */
     document.body.appendChild(container);
+
+    div.style.width = divwidth + "px"; /* esto es repetitivo, debo buscar la forma de que se haga solo una vez, cuando se termine de loopear, porque fuera del loop no me reconoce el div */
+    div.style.height = divwidth + "px";
+    div.style.opacity = Math.random(100);
+
 }
+
+
+
+
+
+
 
