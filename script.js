923fbca568df2150cc85c55887ba78baa1504b18
Luciano Candotti <candotti@Mac-de-Luciano.local>
2025-01-18 13:12:53 -0300

    solucionado resolucion y cantidad

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 /* Vamos a crear en un contenedor flex, una grilla de cuadrados 
-en la que su cantidad se ajusta a la ventana del ordenador */
+en la que su cantidad y tamaño se ajusta a la ventana del ordenador */
 
 
 //tomamos las dimensiones de la ventana del usuario
 const divwidth = Math.round(window.innerWidth / 10);
-const filas = window.innerHeight / divwidth;
+const filas = Math.round(window.innerHeight / divwidth);
 // const ancho = window.innerWidth / 100;
 // const alto = window.innerHeight / 100;
 const resolucion = 10 * filas;
-console.log(divwidth,resolucion);
+console.log(divwidth,resolucion,filas);
 
 
 
@@ -27,10 +27,9 @@ for (let step = 0; step < resolucion; step++) {
     container.appendChild(div); /* ponemos a div como hijo de container */
     document.body.appendChild(container);
 
-    div.style.width = divwidth + "px"; /* esto es repetitivo, debo buscar la forma de que se haga solo una vez, cuando se termine de loopear, porque fuera del loop no me reconoce el div */
+    div.style.width = divwidth + "px"; /* tiene que hacerse cada vez, porque siempre es un nuevo div en el DOM */
     div.style.height = divwidth + "px";
     div.style.opacity = Math.random(100);
-
 }
 
 
